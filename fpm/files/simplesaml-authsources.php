1440e37eb6a299ea071cefd6141dd4d38bb1321c
Nils Vogels <bacardicoke@gmail.com>
2019-06-25 15:58:20 +0200

    Split into two containers

---
+++ fpm/files/simplesaml-authsources.php
@@ -0,0 +1,32 @@
+<?php
+
+$config = [
+
+    // This is a authentication source which handles admin authentication.
+    'admin' => [
+        // The default is to use core:AdminPassword, but it can be replaced with
+        // any authentication source.
+
+        'core:AdminPassword',
+    ],
+
+
+    // An authentication source which can authenticate against both SAML 2.0
+    // and Shibboleth 1.3 IdPs.
+    'default-sp' => [
+        'exampleauth:UserPass',
+
+        'remember.username.enabled' => true,
+        'remember.username.checked' => true,
+
+        'student:studentpass' => [
+            'uid' => ['test'],
+            'eduPersonAffiliation' => ['member', 'student'],
+        ],
+        'employee:employeepass' => [
+            'uid' => ['employee'],
+            'eduPersonAffiliation' => ['member', 'employee'],
+        ],
+    ],
+
+];
